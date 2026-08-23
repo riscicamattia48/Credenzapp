6609dbc4e9df8ce7b11f632033ae248e7342ce04
riscicamattia48 <riscicamattia@gmail.com>
2026-08-23 11:36:57 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'credenzapp-cache-v13';
+const CACHE = 'credenzapp-cache-v15';
 const ASSETS = ['./', './index.html', './app.js', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', event => {
